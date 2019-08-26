65b98a12f300bd6b26e4ea6300fd530384892d3a
Khanh Vu <vtkhanh90@gmail.com>
2019-08-26 23:06:32 +0100

    Add OutputPane

---
+++ PinnacleCodingConvention/Common/PackageGuid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PinnacleCodingConvention.Common
+{
+    internal static class PackageGuid
+    {
+        public const string PinnacleCodingConventionPackageString = "2aef4ddf-562d-4264-9929-24f3be06c921";
+        public const string PinnacleCodingConventionOutputPaneString = "baf6f569-4d9d-4d08-9cf9-6b98cd7a47b1";
+        public const string CleanUpCommandString = "a35bce14-852c-484e-a8c0-88371dbc25ad";
+
+        public static Guid CleanUpCommandGuid = new Guid(CleanUpCommandString);
+        public static Guid PinnacleCodingConventionOutputPaneGuid = new Guid(PinnacleCodingConventionOutputPaneString);
+    }
+}
---
+++ PinnacleCodingConvention/Helpers/OutputWindowHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using PinnacleCodingConvention.Common;
+
+namespace PinnacleCodingConvention.Helpers
+{
+    internal static class OutputWindowHelper
+    {
+        private const string OUTPUT_PANE_TITLE = "Pinnacle Coding Convention";
+        private static IVsOutputWindowPane _outputWindowPane;
+
+        private static IVsOutputWindowPane OutputWindowPane => _outputWindowPane ?? (_outputWindowPane = GetOutputPane());
+
+        public static void WriteLine(string message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (OutputWindowPane is object)
+            {
+                string outputMessage = $"[{DateTime.Now.ToString("hh:mm:ss tt")}] {message}{Environment.NewLine}";
+                OutputWindowPane.OutputString(outputMessage);
+            }
+        }
+
+        private static IVsOutputWindowPane GetOutputPane()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (!(Package.GetGlobalService(typeof(SVsOutputWindow)) is IVsOutputWindow outputWindow))
+                return null;
+
+            Guid outputPaneGuid = PackageGuid.PinnacleCodingConventionOutputPaneGuid;
+
+            outputWindow.CreatePane(ref outputPaneGuid, OUTPUT_PANE_TITLE, fInitVisible: 1, fClearWithSolution: 1);
+            outputWindow.GetPane(ref outputPaneGuid, out IVsOutputWindowPane windowPane);
+
+            return windowPane;
+        }
+
+    }
+}
