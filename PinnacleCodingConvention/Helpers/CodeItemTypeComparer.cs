5bcb02e956ca6a120f277b9f8cf1b811c088a20a
Quoc Nguyen (Brian) <ngminhkuoc@yahoo.com>
2019-11-09 13:00:16 +0000

    Create 2 options for configuration and apply 1 option in ordering by

--- PinnacleCodingConvention/Helpers/CodeItemTypeComparer.cs
+++ PinnacleCodingConvention/Helpers/CodeItemTypeComparer.cs
@@ -1,5 +1,5 @@
-﻿using EnvDTE;
-using PinnacleCodingConvention.Models.CodeItems;
+﻿using PinnacleCodingConvention.Models.CodeItems;
+using PinnacleCodingConvention.Options;
 using System.Collections.Generic;
 
 namespace PinnacleCodingConvention.Helpers
@@ -12,7 +12,7 @@ namespace PinnacleCodingConvention.Helpers
         private readonly bool _secondaryOrderByName;
 
         internal CodeItemTypeComparer() : this(true) { }
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CodeItemTypeComparer"/> class.
         /// </summary>
@@ -68,7 +68,7 @@ namespace PinnacleCodingConvention.Helpers
 
         private static int CalculateTypeOffset(BaseCodeItem codeItem)
         {
-            var itemsOrder = new List<KindCodeItem> 
+            var itemsOrder = new List<KindCodeItem>
             {
                 KindCodeItem.Field,
                 KindCodeItem.Constructor,
@@ -82,7 +82,7 @@ namespace PinnacleCodingConvention.Helpers
         private static int CalculateConstantOffset(BaseCodeItem codeItem)
         {
             var codeItemField = codeItem as CodeItemField;
-            if (codeItemField == null) 
+            if (codeItemField == null)
                 return 0;
 
             return codeItemField.IsConstant ? 0 : 1;
@@ -91,7 +91,7 @@ namespace PinnacleCodingConvention.Helpers
         private static int CalculateReadOnlyOffset(BaseCodeItem codeItem)
         {
             var codeItemField = codeItem as CodeItemField;
-            if (codeItemField == null) 
+            if (codeItemField == null || !GeneralOptions.Instance.OrderByAccessLevelFirst)
                 return 0;
 
             return codeItemField.IsReadOnly ? 0 : 1;
---
+++ PinnacleCodingConvention/Options/GeneralOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace PinnacleCodingConvention.Options
+{
+    internal class GeneralOptions : BaseOptionModel<GeneralOptions>
+    {
+        [DisplayName("Separate Constants and Variables")]
+        [Description("Separate class constants and class variables into different regions")]
+        [DefaultValue(false)]
+        public bool IsSeparateConstAndVar { get; set; }
+
+
+        [DisplayName("Order By Access Level first")]
+        [Description("Order the class variables by Access Level then Name")]
+        [DefaultValue(true)]
+        public bool OrderByAccessLevelFirst { get; set; }
+    }
+}
